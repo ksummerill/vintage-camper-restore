281bdc2a77ba7f8e59e2c2dc2232423060b83d2b
kaitlyn summerill <kaitlynsummerill@gmail.com>
2021-02-11 08:22:32 -0800

    made project input form, addProject action and updated the project reducer

--- src/components/ProjectInput.js
+++ src/components/ProjectInput.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addProject} from '../actions/addProject'
 
 class ProjectInput extends React.Component {
+
+  state = {name: ''}
+
+  handleChange = (event) => {
+    this.setState({
+        name: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addProject(this.state);
+    this.setState({
+      name: ''
+    });
+  }
 
   render() {
     return (
       <div>
-        ProjectInput
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Project Name</label>
+          <input type="text" name="name" value={this.state.name} onChange={(event) => this.handleChange(event)} placeholder="Project Name"/>
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
     )
   }
 }
 
-export default ProjectInput
+
+export default connect(null, {addProject})(ProjectInput)
--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -5,6 +5,8 @@ export default function projectReducer(state = {projects: []}, action) {
   switch (action.type) {
     case 'FETCH_PROJECTS':
       return {projects: action.payload}
+    case 'ADD_PROJECT':
+      return {...state, projects: [...state.projects, action.payload]}
     default:
       return state
   }
